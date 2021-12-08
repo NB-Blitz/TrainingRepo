2e2ea4dd2dd31b992b6a68695de57f91d143a7b1
DigiWorm <aus343@gmail.com>
2021-12-07 20:03:03 -0600

    Practice Junk

--- src/main/java/org/team5148/training/Robot.java
+++ src/main/java/org/team5148/training/Robot.java
@@ -1,11 +1,13 @@
 package org.team5148.training;
 
+import com.kauailabs.navx.frc.AHRS;
 import com.revrobotics.CANSparkMax;
-import com.revrobotics.SparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
-import edu.wpi.first.wpilibj.Spark;
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Robot extends TimedRobot {
 
@@ -13,15 +15,46 @@ public class Robot extends TimedRobot {
 	CANSparkMax frontRight = new CANSparkMax(3, MotorType.kBrushless);
 	CANSparkMax backLeft = new CANSparkMax(2, MotorType.kBrushless);
 	CANSparkMax backRight = new CANSparkMax(4, MotorType.kBrushless);
+	Joystick bill = new Joystick(3);
+	Timer timer = new Timer();
+	AHRS navx = new AHRS();
 
 	@Override
 	public void autonomousInit() {
-
+		timer.reset();
+		timer.start();
 	}
 
 	@Override
 	public void autonomousPeriodic() {
+		double angle = navx.getFusedHeading();
+		double time = timer.get();
 
+		SmartDashboard.putNumber("Angle", angle);
+
+		if (time <= 1) {
+			frontLeft.set(-0.2);
+			frontRight.set(0.2);
+			backLeft.set(-0.2);
+			backRight.set(0.2);
+		} else if (time >= 1 && time <= 6) {
+			if (angle <= 90) {
+				frontLeft.set(-0.2);
+				frontRight.set(0.2);
+				backLeft.set(0.2);
+				backRight.set(-0.2);
+			} else {
+				frontLeft.set(0);
+				frontRight.set(0);
+				backLeft.set(0);
+				backRight.set(0);
+			}
+		} else {
+			frontLeft.set(0);
+			frontRight.set(0);
+			backLeft.set(0);
+			backRight.set(0);
+		}
 	}
 
 	@Override
@@ -30,9 +63,15 @@ public class Robot extends TimedRobot {
 
 	@Override
 	public void teleopPeriodic() {
-		frontLeft.set(-0.1);
-		frontRight.set(0.1);
-		backLeft.set(-0.1);
-		backRight.set(0.1);
+		double y = -bill.getY();
+		double x = bill.getX();
+		double z = -bill.getZ();
+		double slider = bill.getThrottle();
+		double speed = 0.2 * (-slider + 1) / 2;
+
+		frontLeft.set(-speed * (x + y - z));
+		frontRight.set(speed * (-x + y - z));
+		backLeft.set(-speed * (-x + y + z));
+		backRight.set(speed * (x + y + z));
 	}
 }
