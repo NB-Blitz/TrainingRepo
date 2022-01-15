ca766137cb622157e7d52007a5d40b12692891d4
DigiWorm <aus343@gmail.com>
2022-01-15 04:22:31 -0600

    Added DriveTeam Junk

--- src/main/java/org/team5148/training/CANTSparkMax.java
+++
@@ -1,9 +0,0 @@
-package org.team5148.training;
-
-import com.revrobotics.CANSparkMax;
-
-public class CANTSparkMax extends CANSparkMax {
-	public CANTSparkMax(int id, MotorType motorType) {
-		super(id, motorType);
-	}
-}
--- src/main/java/org/team5148/training/Robot.java
+++ src/main/java/org/team5148/training/Robot.java
@@ -2,107 +2,98 @@ package org.team5148.training;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
-import com.kauailabs.navx.frc.AHRS;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
+import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.GenericHID.Hand;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 
 public class Robot extends TimedRobot {
 
-	CANTSparkMax frontLeft = new CANTSparkMax(1, MotorType.kBrushless);
-	CANTSparkMax frontRight = new CANTSparkMax(3, MotorType.kBrushless);
-	CANTSparkMax backLeft = new CANTSparkMax(2, MotorType.kBrushless);
-	CANTSparkMax backRight = new CANTSparkMax(4, MotorType.kBrushless);
-	CANTSparkMax tallGun = new CANTSparkMax(5, MotorType.kBrushless);
-	CANTSparkMax babyGun = new CANTSparkMax(6, MotorType.kBrushless);
-	TalonSRX ReloadJohn = new TalonSRX(8);
-	CANTSparkMax ReloadBilly = new CANTSparkMax(9, MotorType.kBrushless);
-	CANTSparkMax ReloadBobby = new CANTSparkMax(10, MotorType.kBrushless);
-	CANTSparkMax ReloadJohnny = new CANTSparkMax(11, MotorType.kBrushless);
-	Joystick bill = new Joystick(3);
-	Timer timer = new Timer();
-	AHRS navx = new AHRS();
+	// Constants
+	private final double ROBOT_SPEED = 1;
+	private final double LAUNCHER_SPEED = 0.8;
+	private final double STORAGE_SPEED = 0.8;
+	private final double INTAKE_SPEED = 0.25;
 
-	@Override
-	public void autonomousInit() {
-		timer.reset();
-		timer.start();
-		navx.reset();
-	}
+	// Drivetrain
+	CANSparkMax frontLeft = new CANSparkMax(1, MotorType.kBrushless);
+	CANSparkMax frontRight = new CANSparkMax(3, MotorType.kBrushless);
+	CANSparkMax backLeft = new CANSparkMax(2, MotorType.kBrushless);
+	CANSparkMax backRight = new CANSparkMax(4, MotorType.kBrushless);
 
-	@Override
-	public void autonomousPeriodic() {
-		double angle = (navx.getAngle() + 180) % 360;
-		double time = timer.get();
+	// Launcher
+	CANSparkMax topLauncher = new CANSparkMax(5, MotorType.kBrushless);
+	CANSparkMax bottomLauncher = new CANSparkMax(6, MotorType.kBrushless);
 
-		SmartDashboard.putNumber("Angle", angle);
+	// Storage
+	TalonSRX launcherFeeder = new TalonSRX(8);
+	CANSparkMax storageFeeder1 = new CANSparkMax(9, MotorType.kBrushless);
+	CANSparkMax storageFeeder2 = new CANSparkMax(10, MotorType.kBrushless);
+	CANSparkMax intake = new CANSparkMax(11, MotorType.kBrushless);
 
-		if (time <= 1) {
-			frontLeft.set(-0.2);
-			frontRight.set(0.2);
-			backLeft.set(-0.2);
-			backRight.set(0.2);
-		} else if (time >= 1 && time <= 6) {
-			if (angle <= 270) {
-				frontLeft.set(-0.2);
-				frontRight.set(-0.2);
-				backLeft.set(-0.2);
-				backRight.set(-0.2);
-			} else {
-				frontLeft.set(0);
-				frontRight.set(0);
-				backLeft.set(0);
-				backRight.set(0);
-			}
-		} else {
-			frontLeft.set(0);
-			frontRight.set(0);
-			backLeft.set(0);
-			backRight.set(0);
-		}
-	}
+	// Controls
+	XboxController xboxController = new XboxController(0);
+	Joystick joystick = new Joystick(1);
+
+	// Shuffleboard
+	ShuffleboardTab tab = Shuffleboard.getTab("Practice");
+	NetworkTableEntry useXbox = tab.add("Use Xbox Controller", true).getEntry();
 
 	@Override
 	public void teleopInit() {
+
 	}
 
 	@Override
 	public void teleopPeriodic() {
-		double y = -bill.getY();
-		double x = bill.getX();
-		double z = -bill.getZ();
-		double slider = bill.getThrottle();
-		double speed = 0.2 * (-slider + 1) / 2;
-		boolean BBBJTTL = bill.getRawButton(1); // BBBJTL = billy bob boe joe the third L
-		boolean BBBBTFIL = bill.getRawButton(2); // BBBBTFIL = bobby billy bob bill the fifteenth IL
-		boolean JJJJJTTTXXXXVIII = true; // JJJJJTTTXXXXVIII = JimmyJaqavienceJohnJohnathanJimmythonTheThirtyThird
-											// XXXXVIII
 
-		frontLeft.set(-speed * (x + y - z));
-		frontRight.set(speed * (-x + y - z));
-		backLeft.set(-speed * (-x + y + z));
-		backRight.set(speed * (x + y + z));
-		if (BBBJTTL == true) {
-			tallGun.set(-1);
-			babyGun.set(1);
+		// Input Variables
+		boolean isXbox = useXbox.getBoolean(true);
+		double xInput = 0;
+		double yInput = 0;
+		double zInput = 0;
+		boolean launcherInput = false;
+		boolean intakeInput = false;
+
+		// Input Setting
+		if (isXbox) {
+			xInput = xboxController.getX(Hand.kLeft);
+			yInput = xboxController.getY(Hand.kLeft);
+			zInput = xboxController.getX(Hand.kRight);
+
+			launcherInput = xboxController.getBButton();
+			intakeInput = xboxController.getAButton();
 		} else {
-			tallGun.set(0);
-			babyGun.set(0);
+			xInput = joystick.getX();
+			yInput = joystick.getY();
+			zInput = joystick.getZ();
+
+			launcherInput = joystick.getRawButton(1);
+			intakeInput = joystick.getRawButton(2);
 		}
-		if (BBBBTFIL == true) {
-			ReloadBilly.set(-0.8);
-			ReloadBobby.set(-0.8);
-			ReloadJohnny.set(0.4);
-			ReloadJohn.set(ControlMode.PercentOutput, -0.4);
-		} else {
-			ReloadBilly.set(0);
-			ReloadBobby.set(0);
-			ReloadJohnny.set(0);
-			ReloadJohn.set(ControlMode.PercentOutput, 0);
-		}
+
+		// Drive Train
+		frontLeft.set(-ROBOT_SPEED * (xInput + yInput - zInput));
+		frontRight.set(ROBOT_SPEED * (-xInput + yInput - zInput));
+		backLeft.set(-ROBOT_SPEED * (-xInput + yInput + zInput));
+		backRight.set(ROBOT_SPEED * (xInput + yInput + zInput));
+
+		// Launcher
+		topLauncher.set(launcherInput ? LAUNCHER_SPEED : 0);
+		bottomLauncher.set(launcherInput ? LAUNCHER_SPEED : 0);
+
+		// Storage
+		launcherFeeder.set(ControlMode.PercentOutput, intakeInput ? STORAGE_SPEED : 0);
+		storageFeeder1.set(intakeInput ? STORAGE_SPEED : 0);
+		storageFeeder2.set(intakeInput ? STORAGE_SPEED : 0);
+
+		// Intake
+		intake.set(intakeInput ? INTAKE_SPEED : 0);
 	}
 }
